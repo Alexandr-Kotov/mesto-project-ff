e238c533985a881828a8d78cc73b9b66e79bf8d0
Kotov Alexandr <Mr1Hiroshi@yandex.ru>
2024-05-23 20:18:26 +0500

    Projecet

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# Проектная работа Mesto
+# Проект: Закрывающий тег
 
+## Описание проекта.
+Спринт №5 от Яндекс.Практикум
+* Создание, добавление и удаление элементов в DOM.
+* Методы работы с данными.
+* Работа с масивами.
+* Работа с функциями.
+
+## Используемые технологии
+* HTML
+* CSS
+* JavaScript
+
+## Ссылки на проект
+
+* [репозиторий] (https://github.com/Alexandr-Kotov/mesto-project-ff)
+
+* [Ссылка на макет в Figma](https://www.figma.com/file/bjyvbKKJN2naO0ucURl2Z0/JavaScript.-Sprint-5?node-id=0%3A1)
+
+* [Ссылка на сайт Закрывающий тег](https://alexandr-kotov.github.io/mesto-project-ff/)
--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,20 @@
-// @todo: Темплейт карточки
 const cardsContainer = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
-initialCards.forEach(function (element){
-  const cardTemplate = document.querySelector('#card-template').content;
+function createCard (element) {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-
   cardElement.querySelector('.card__image').src = element.link;
   cardElement.querySelector('.card__image').alt = element.name;
   cardElement.querySelector('.card__title').textContent = element.name;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', deletCard);
+  return cardElement
+}
 
-  const deleteButton = cardElement.querySelector('.card__delete-button');
+function deletCard (event) {
+  const deletElement = event.target.closest('.card')
+  deletElement.remove();
+}
 
-  deleteButton.addEventListener('click', function () {
-    const cardItem = deleteButton.closest('.card');
-    cardItem.remove();
-  }); 
-
-  cardsContainer.append(cardElement)
+initialCards.forEach(function (element) {
+  cardsContainer.append(createCard(element))
 })
-
-// @todo: DOM узлы
-// @todo: Функция создания карточки
-// @todo: Функция удаления карточки
-// @todo: Вывести карточки на страницу
