eef46efb5bbc73fdfdf571dd84f27429541d1cd5
Alexandr <Mr1Hiroshi@yandex.ru>
2024-05-23 16:09:56 +0500

    CardTemplate

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
 // @todo: Темплейт карточки
+const cardsContainer = document.querySelector('.places__list');
+
+initialCards.forEach(function (element){
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+  cardElement.querySelector('.card__image').src = element.link;
+  cardElement.querySelector('.card__image').alt = element.name;
+  cardElement.querySelector('.card__title').textContent = element.name;
+
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  deleteButton.addEventListener('click', function () {
+    const cardItem = deleteButton.closest('.card');
+    cardItem.remove();
+  }); 
+
+  cardsContainer.append(cardElement)
+})
 
 // @todo: DOM узлы
-
 // @todo: Функция создания карточки
-
 // @todo: Функция удаления карточки
-
 // @todo: Вывести карточки на страницу
